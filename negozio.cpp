ec78c4b450bd16f1f41ed55a8dc5e06744fe6d56
gayaconlay <gaia.parazza2@studio.unibo.it>
2023-11-01 16:56:28 +0100

    creazione prime funzioni del negozio (non so se ci siano errori perche non ho fatto make)

--- negozio.cpp
+++ negozio.cpp
@@ -1,8 +1,14 @@
-
-/*
 #include "hpp-files/negozio.hpp"
 
-Negozio::negozio(WINDOW* win, WINDOW* menuuwin) {
+Negozio::Negozio(WINDOW* win, InfoPlayer* InfoPlayer) {
+    win=win;
+    InfoPlayer  = entitiesOBJ->ReturnPlayerOBJ();
+}
+
+void werase(WINDOW *win){ werase(win) }
+
+void create_Shop(WINDOW *win) {
+
+}
 
 	
-}*/
