fe1f6789a4e4662e38e6c9828230fac2a24ce723
gayaconlay <gaia.parazza2@studio.unibo.it>
2023-11-03 18:26:38 +0100

    dichiaraate funzioni negozio e aggiunto 3 mappe vuote nel file txt da completare appena ho dettagli su mappa

--- negozio.cpp
+++ negozio.cpp
@@ -1,14 +1,32 @@
-/*#include "hpp-files/negozio.hpp"
-
+#include "hpp-files/negozio.hpp"
+#include <string>
+/*
 Negozio::Negozio(WINDOW* win, InfoPlayer* InfoPlayer) {
     win=win;
     InfoPlayer  = entitiesOBJ->ReturnPlayerOBJ();
 }
 
-void werase(WINDOW *win){ werase(win) }
+void Negozio::werase(WINDOW *win){ werase(win); }
 
-void create_Shop(WINDOW *win) {
+void Negozio::create_Shop(WINDOW *win) {
+//coi punti cosa faccio?
+
+std::string scelte[2]={"ACQUISTA COLPI", "ACQUISTA VITE"};
+}
+
+void Negozio::buyshots(){   //10 spari 20 monete?
 
 }
+
+void Negozio::buyLives(){   //10 monete 1 vita -> vita infinita?
+
+}
+
+
+void Negozio::colorPlayer(){   //con 500 punti si puo cambiare colore al player?
+std::string colori[4]={"Blue" ;"Green", "Yellow"; "Red"};
+}
+
+
 */
 	
